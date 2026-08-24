7850f06f930980c3aa86a00d023d995411c21c11
v4300856-create <v4300856@gmail.com>
2026-08-25 03:17:51 +1000

    Add license validation startup flow

--- csharp-v1/App.xaml.cs
+++ csharp-v1/App.xaml.cs
@@ -1,3 +1,43 @@
 using System.Windows;
+
 namespace RiuClicker;
-public partial class App : Application { }
+
+public partial class App : Application
+{
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+        var savedKey = LicenseService.LoadSavedKey();
+        if (!string.IsNullOrWhiteSpace(savedKey))
+        {
+            var check = await LicenseService.ValidateAsync(savedKey);
+            if (check.Ok)
+            {
+                OpenMainWindow();
+                return;
+            }
+
+            LicenseService.ClearSavedKey();
+        }
+
+        var activation = new ActivationWindow();
+        var ok = activation.ShowDialog() == true && activation.Activated;
+        if (!ok)
+        {
+            Shutdown();
+            return;
+        }
+
+        OpenMainWindow();
+    }
+
+    private void OpenMainWindow()
+    {
+        var main = new MainWindow();
+        MainWindow = main;
+        ShutdownMode = ShutdownMode.OnMainWindowClose;
+        main.Show();
+    }
+}
